17c1f0708a8bb44f99961ef9a6b8da2489422a45
shamanyu <shubham@springboard.com>
2021-04-25 10:21:32 +0530

    Make it all work

--- final/src/App.js
+++ final/src/App.js
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
 
+	const QUESTION_API = 'https://opentdb.com/api.php?amount=5&type=multiple';
+	const [questions, setquestions] = useState([]);
+    const [isBusy, setBusy] = useState(true);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
@@ -56,27 +22,71 @@ export default function App() {
 			setShowScore(true);
 		}
 	};
-	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-	);
+
+	const transformRawQuestions = (rawQuestions) => {
+	    let transformedQuestions = [];
+        rawQuestions.results.forEach(function(item, index) {
+            console.log("Parsing...", item, index);
+            let question = {}
+            question.category = item.category;
+            question.questionText = item.question;
+            question.answerOptions = [];
+            item.incorrect_answers.forEach(function(inner_item, inner_index) {
+                question.answerOptions.push(
+                    {
+                        'answerText': inner_item,
+                        'isCorrect': false,
+                    }
+                )
+            });
+            question.answerOptions.push(
+                {
+                    'answerText': item.correct_answer,
+                    'isCorrect': true,
+                }
+            );
+            question.answerOptions = question.answerOptions.sort(() => Math.random() - 0.5)
+            transformedQuestions.push(question);
+        });
+        return transformedQuestions;
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                QUESTION_API
+            );
+            setquestions(transformRawQuestions(result.data));
+            setBusy(false);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className='app'>
+            {showScore ? (
+                <div className='score-section'>
+                    You scored {score} out of {questions.length}
+                </div>
+            ) : ( !isBusy ? (
+                    <>
+                        <div className='question-section'>
+                            <div className='question-count'>
+                                <span>Question {currentQuestion + 1}</span>/{questions.length}
+                            </div>
+                            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                        </div>
+                        <div className='answer-section'>
+                            {questions[currentQuestion].answerOptions.map((answerOption) => (
+                                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <div>Loading</div>
+                )
+
+            )}
+        </div>
+    );
 }
